11f08ea33625da5ddac8bc098eb1f4d75e8d7447
Rami <rami.aouinti@gmail.com>
2026-02-20 00:39:36 +0100

    Handle DB connection failures on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PlatformRepository;
+use Doctrine\DBAL\Exception as DbalException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,10 +15,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PlatformRepository $platformRepository): Response
     {
+        $platforms = [];
+        $databaseUnavailable = false;
+
+        try {
+            $platforms = $platformRepository->findBy([], ['id' => 'ASC']);
+        } catch (DbalException) {
+            $databaseUnavailable = true;
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'platforms' => $platforms,
+            'database_unavailable' => $databaseUnavailable,
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Platform;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 100; ++$i) {
+            $platform = new Platform();
+            $platform->setName(sprintf('Platform %d', $i));
+            $platform->setDescription(sprintf('Description de la platform %d', $i));
+
+            $manager->persist($platform);
+        }
 
         $manager->flush();
     }
