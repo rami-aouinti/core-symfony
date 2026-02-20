f358004ce2c115ec4cff558023d67b892586d61d
Rami <rami.aouinti@gmail.com>
2026-02-20 02:16:47 +0100

    Refactor platform creation flow through application handler

--- src/Controller/PlatformController.php
+++ src/Controller/PlatformController.php
@@ -4,9 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Platform;
 use App\Form\PlatformType;
+use App\Platform\Application\Command\CreatePlatformCommand;
+use App\Platform\Application\Handler\CreatePlatformHandler;
+use App\Platform\Domain\Exception\InvalidPlatformException;
 use App\Repository\PlatformRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,21 +31,53 @@ final class PlatformController extends AbstractController
     }
 
     #[Route('/new', name: 'app_platform_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, CreatePlatformHandler $createPlatformHandler): Response
     {
-        $platform = new Platform();
-        $form = $this->createForm(PlatformType::class, $platform);
+        $form = $this->createFormBuilder([
+            'name' => '',
+            'description' => '',
+            'color' => '',
+            'active' => false,
+        ])
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('color', ColorType::class, [
+                'required' => false,
+            ])
+            ->add('active', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->getForm();
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($platform);
-            $entityManager->flush();
+            /** @var array{name: string|null, description: string|null, color: string|null, active: bool|null} $data */
+            $data = $form->getData();
+
+            $command = new CreatePlatformCommand(
+                name: (string) ($data['name'] ?? ''),
+                description: (string) ($data['description'] ?? ''),
+                color: (string) ($data['color'] ?? ''),
+                active: (bool) ($data['active'] ?? false),
+            );
+
+            try {
+                $createPlatformHandler($command);
+            } catch (InvalidPlatformException $exception) {
+                $form->addError(new FormError($exception->getMessage()));
+
+                return $this->render('platform/new.html.twig', [
+                    'form' => $form,
+                ]);
+            }
 
             return $this->redirectToRoute('app_platform_index');
         }
 
         return $this->render('platform/new.html.twig', [
-            'platform' => $platform,
             'form' => $form,
         ]);
     }
